c4c7a16ad43e1fe515e4b60000388e645fd492bc
wangxin0 <wangxin0@yonyou.com>
2015-03-02 16:46:27 +0800

    ffsdf

--- test/net/loyin/test/TestClz.java
+++ test/net/loyin/test/TestClz.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.StringUtils;
 public class TestClz {
 
 	public static void main(String[] args) {
+		String test = "ff";
 		String s=null,s1="";
 		System.out.println(
 				StringUtils.isNoneEmpty(s)+
