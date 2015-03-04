92bac726a00530e9447f061d64811d57d668a601
luxn0429 <luxn0429@gmail.com>
2015-03-04 16:56:38 +0800

    sdf

---
+++ ROOT/app/plan/plant/view.js
@@ -0,0 +1,31 @@
+var api = frameElement.api,oper = api.data.oper,id=api.data.id,$_form=$("#base_form"),url=rootPath+"/plan/plant";
+var model = avalon.define({$id:'view',
+	data:{name:"",variety:"",area:"",plant_region:"",spawning_time:"",seeding_time:"",growing_time:"",ripe_time:"",harvest_time:"",fertilizer:"",pestisaid:"",head_name:"",company_id:"",company_name:"",id:"",mobile:""},
+    tabActive:0,
+    showTab:function(i,b){
+    	model.tabActive=i;
+    }
+});
+model.data.$watch("$all",function(name,a,b){
+	if(b==null||b=="null"){
+		eval("model.data."+name+"='';");
+	}
+});
+
+var THISPAGE = {
+	init : function() {
+		this.initDom();
+	},
+	initDom : function() {
+		if(id!=undefined&&id!=''&&id!='undefined'){
+			Public.ajaxPost(url+"/qryOp.json",{id:id}, function(json){
+				if(json.status==200){
+					model.data=json.data;
+				}else{
+					parent.Public.tips({type: 1, content : json.msg});
+				}
+			});
+		}
+	}
+};
+THISPAGE.init();
